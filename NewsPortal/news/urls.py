d1285cd512ae51a50b42bd39a74c324eb6db57e5
sed-dan <danilchenko.serge@outlook.com>
2026-03-30 13:25:05 +0300

    Delete NewsPortal directory

--- NewsPortal/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import PostListView, PostDetailView
-
-
-urlpatterns = [
-    path("", PostListView.as_view(), name='post_list'),
-    path("<int:pk>/", PostDetailView.as_view(), name='post_detail'),
-]
